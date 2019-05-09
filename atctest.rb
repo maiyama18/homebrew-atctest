a391531830a2d2995265e0953180c7048441ab43
muiscript <myam0118@gmail.com>
2019-05-09 12:04:42 +0900

    update tar.gz url

--- atctest.rb
+++ atctest.rb
@@ -4,7 +4,7 @@
 class Atctest < Formula
   desc "command line tool to check if your code passes the sample problems on atcoder"
   homepage "https://github.com/mui87/atctest"
-  url "mui87/atctest"
+  url "https://github.com/mui87/atctest/releases/download/v0.9.0/atctest"
   version "v0.9.0"
   sha256 "1fe2dd7b20cb22a51eb3399a6160fd8de7978a70cbe3624902ccc25c6b52a95c"
 
